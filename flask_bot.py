5afd71167ee9209fb21ac23a6f75f28101fdbe44
ROHIT <74254913+Rohit-78958@users.noreply.github.com>
2023-10-24 18:39:49 +0530

    Update flask_bot.py

--- flask_bot.py
+++ flask_bot.py
@@ -1,16 +1,27 @@
 from flask import Flask, request
-import gdown
-import subprocess
+from nbconvert.preprocessors import ExecutePreprocessor
+from nbformat import read, write
 
 app = Flask(__name)
 
 @app.route('/execute_notebook', methods=['POST'])
 def execute_notebook():
-    notebook_link = request.form.get('notebook_link')
+    notebook_link = request.form.get('https://colab.research.google.com/drive/1mgUWEgPIcxooLXjgaT9mgYshRnBgTFeC?usp=sharing')
 
-    gdown.download(notebook_link, output='notebook.ipynb', quiet=False)
+    # Download the notebook content (if needed) and read it
+    # Replace this with the code to fetch the notebook content
+    notebook_content = fetch_notebook_content(notebook_link)
 
-    subprocess.run(['jupyter', 'nbconvert', '--to', 'html', 'notebook.ipynb'])
+    # Read the notebook
+    notebook = read(notebook_content, as_version=4)
+
+    # Create an execution preprocessor
+    execute_preprocessor = ExecutePreprocessor(timeout=None, kernel_name='python3')
+
+    # Execute the notebook in-memory
+    execute_preprocessor.preprocess(notebook, {})
+
+    # The notebook is now executed in-memory, but not saved
 
     return 'Notebook execution complete'
 
